55c87ec6bf8fa223f91ad185d6f115b09c5ac8ef
Henri Delozanne <hnr.delozanne@gmail.com>
2024-10-10 17:52:12 +0200

    feat(find-and-replace): add page with main feature

---
+++ pages/find-and-replace.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-textarea v-model="text" label="Original text" />
+    <v-text-field
+      v-model="origin" label="Search value"
+    />
+    <v-text-field
+      v-model="replaced" label="Replace value"
+    />
+    <v-textarea v-model="result" label="Updated text" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const text : Ref<string> = ref('');
+const origin: Ref<string> = ref('');
+const replaced: Ref<string> = ref('');
+
+const result : ComputedRef<string> = computed(() => text.value.replaceAll(origin.value, replaced.value));
+</script>

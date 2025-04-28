c645590466fdaa0d8729ac6639e1196c48556cbf
Henri Delozanne <hnr.delozanne@gmail.com>
2025-04-28 17:30:05 +0200

    Remove darkmode

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -8,21 +8,13 @@
     <template v-if="mobile" #prepend>
       <v-app-bar-nav-icon />
     </template>
-    <template #append>
-      <v-btn icon="mdi-theme-light-dark" @click="toggleTheme" />
-    </template>
   </v-app-bar>
 </template>
 
 <script setup>
-import { useDisplay, useTheme } from 'vuetify';
+import { useDisplay } from 'vuetify';
 
 const { mobile } = useDisplay();
-const theme = useTheme();
-
-function toggleTheme () {
-  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark';
-}
 </script>
 
 <style>

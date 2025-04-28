7886810842cb311945867a7a942aa0c7c815b029
Henri Delozanne <hnr.delozanne@gmail.com>
2025-04-28 18:16:22 +0200

    Enhance UX of case converter page

--- pages/case-converter.vue
+++ pages/case-converter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <v-container>
     <v-row class="section">
       <v-col>
         <v-textarea v-model="text" label="Original text" />
@@ -7,68 +7,75 @@
     </v-row>
     <v-row class="section">
       <v-col class="btn-container">
-        <v-btn @click="convertToUpperCase">
+        <v-btn :class="{selected: selectedTransformer === 'upper'}" @click="convertToUpperCase">
           Upper case
         </v-btn>
-        <v-btn @click="convertToLowerCase">
+        <v-btn :class="{selected: selectedTransformer === 'lower'}" @click="convertToLowerCase">
           Lower case
         </v-btn>
-        <v-btn @click="convertToCapitalized">
+        <v-btn :class="{selected: selectedTransformer === 'capitalized'}" @click="convertToCapitalized">
           Capitalized
         </v-btn>
-        <v-btn @click="convertToCamelCase">
+        <v-btn :class="{selected: selectedTransformer === 'camel'}" @click="convertToCamelCase">
           camelCase
         </v-btn>
-        <v-btn @click="convertToSnakeCase">
+        <v-btn :class="{selected: selectedTransformer === 'snake'}" @click="convertToSnakeCase">
           Snake_case
         </v-btn>
-        <v-btn @click="convertToKebabCase">
+        <v-btn :class="{selected: selectedTransformer === 'kebab'}" @click="convertToKebabCase">
           kebab-case
         </v-btn>
-        <v-btn @click="convertToPascalCase">
+        <v-btn :class="{selected: selectedTransformer === 'pascal'}" @click="convertToPascalCase">
           PascalCase
         </v-btn>
-        <v-btn @click="convertToFlatCase">
+        <v-btn :class="{selected: selectedTransformer === 'flat'}" @click="convertToFlatCase">
           flatcase
         </v-btn>
-        <v-btn @click="reverseText">
+        <v-btn :class="{selected: selectedTransformer === 'reverse'}" @click="reverseText">
           Reverse
         </v-btn>
-        <v-btn @click="convertToBinary">
+        <v-btn :class="{selected: selectedTransformer === 'binary'}" @click="convertToBinary">
           Binary
         </v-btn>
       </v-col>
     </v-row>
-    <v-row justify="center">
-      <v-col cols="1" class="centered-action__wrapper">
-        <CopyButton :text-to-copy="result" />
-      </v-col>
-    </v-row>
+
     <v-row class="section">
       <v-col>
         <v-textarea v-model="result" label="Updated text" />
       </v-col>
     </v-row>
-  </div>
+
+    <v-row justify="center">
+      <v-col cols="1" class="centered-action__wrapper">
+        <CopyButton :text-to-copy="result" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
-const text : Ref<string> = ref('');
+const text : Ref<string> = ref('This is an example text');
 const result : Ref<string> = ref('');
+const selectedTransformer = ref('');
 
 const convertToUpperCase = () : void => {
+  selectedTransformer.value = 'upper';
   result.value = text.value.toUpperCase();
 };
 
 const convertToLowerCase = () : void => {
+  selectedTransformer.value = 'lower';
   result.value = text.value.toLowerCase();
 };
 
 const convertToCapitalized = () : void => {
+  selectedTransformer.value = 'capitalized';
   result.value = text.value.split(/(?:\n| )+/).map((word : string) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
 };
 
 const convertToCamelCase = () : void => {
+  selectedTransformer.value = 'camel';
   result.value = text.value.split(/(?:\n| )+/).map((word : string, index: number) => {
     if (index === 0) { return word.toLowerCase(); }
     return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
@@ -76,35 +83,49 @@ const convertToCamelCase = () : void => {
 };
 
 const convertToSnakeCase = () : void => {
+  selectedTransformer.value = 'snake';
   result.value = text.value.trim().replaceAll(' ', '_');
 };
 
 const convertToKebabCase = () : void => {
+  selectedTransformer.value = 'kebab';
   result.value = text.value.trim().replaceAll(' ', '-');
 };
 
 const convertToPascalCase = () : void => {
+  selectedTransformer.value = 'pascal';
   result.value = text.value.split(' ').map((word : string) => word.charAt(0).toUpperCase() + word.slice(1)).join('');
 };
 
 const convertToFlatCase = () : void => {
+  selectedTransformer.value = 'flat';
   result.value = text.value.trim().replaceAll(' ', '').toLowerCase();
 };
 
 const reverseText = () : void => {
+  selectedTransformer.value = 'reverse';
   result.value = text.value.split('').reverse().join('');
 };
 
 const convertToBinary = () : void => {
+  selectedTransformer.value = 'binary';
   result.value = text.value.split('').map(char => char.charCodeAt(0).toString(2)).join(' ');
 };
+
+onMounted(() => {
+  convertToUpperCase();
+});
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .btn-container {
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-around;
-  gap: 12px;
+  gap: 10px;
+
+  .selected {
+    background: linear-gradient(-20deg, #2b5876 0%, #4e4376 100%);
+    color: white;
+  }
 }
 </style>

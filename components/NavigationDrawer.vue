eef993073bb448d9e6f1e4376a88e1fc03cf9a2d
Henri Delozanne <hnr.delozanne@gmail.com>
2025-06-20 09:09:55 +0200

    add icons in menu

--- components/NavigationDrawer.vue
+++ components/NavigationDrawer.vue
@@ -7,6 +7,7 @@
         link
         :title="item.title"
         :to="item.path"
+        :prepend-icon="item.icon"
       />
     </v-list>
   </v-navigation-drawer>
@@ -16,23 +17,28 @@
 const navItems = [
   {
     title: 'Random word generator',
-    path: '/random-word-generator'
+    path: '/random-word-generator',
+    icon: 'mdi-lightbulb-on-outline'
   },
   {
     title: 'Password generator',
-    path: '/password-generator'
+    path: '/password-generator',
+    icon: 'mdi-lock-outline'
   },
   {
     title: 'Case converter',
-    path: '/case-converter'
+    path: '/case-converter',
+    icon: 'mdi-swap-horizontal'
   },
   {
     title: 'Word counter',
-    path: '/word-counter'
+    path: '/word-counter',
+    icon: 'mdi-counter'
   },
   {
     title: 'Find and replace',
-    path: '/find-and-replace'
+    path: '/find-and-replace',
+    icon: 'mdi-find-replace'
   }
 ];
 </script>
